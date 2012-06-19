4af31ca889a7029c14fae3c527d26530078a6a8f
wolfgarnet <wolfgarnet@gmail.com>
2012-06-19 14:43:14 +0200

    Cleaned logging stream

--- src/main/java/net/praqma/logging/LoggingStream.java
+++ src/main/java/net/praqma/logging/LoggingStream.java
@@ -1,16 +1,13 @@
 package net.praqma.logging;
 
-import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.io.PrintStream;
 import java.io.Serializable;
 
 import hudson.CloseProofOutputStream;
 import hudson.remoting.RemoteOutputStream;
-import hudson.util.StreamTaskListener;
 
 public class LoggingStream implements Serializable {
 
@@ -28,15 +25,10 @@ public class LoggingStream implements Serializable {
     	CloseProofOutputStream cpos = new CloseProofOutputStream(this.out);
     	RemoteOutputStream ros = new RemoteOutputStream(cpos);
         out.writeObject( ros );
-        //out.writeObject(charset==null? null : charset.name());
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        //out = new FilterOutputStream((OutputStream)in.readObject());
     	out = (OutputStream)in.readObject();
-        //new FilterOutputStream(out)
-        //String name = (String)in.readObject();
-        //charset = name==null ? null : Charset.forName(name);
     }
 
 }
