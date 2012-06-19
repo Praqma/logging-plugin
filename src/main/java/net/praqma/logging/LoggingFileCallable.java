13f00c0e0a4a3085abd78f50159d9de8e919ff0b
wolfgarnet <wolfgarnet@gmail.com>
2012-06-19 14:41:10 +0200

    Cleaned logging file callable

--- src/main/java/net/praqma/logging/LoggingFileCallable.java
+++ src/main/java/net/praqma/logging/LoggingFileCallable.java
@@ -1,21 +1,11 @@
 package net.praqma.logging;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.List;
-import java.util.logging.Filter;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-import java.util.logging.StreamHandler;
 
 import hudson.FilePath.FileCallable;
-import hudson.model.Item;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Actionable;
@@ -26,7 +16,6 @@ public abstract class LoggingFileCallable<T> implements FileCallable<T> {
 
 	protected LoggingStream lstream;
 	private List<LoggerTarget> targets;
-	private boolean remote = false;
 
 	public LoggingFileCallable( Actionable a ) {
 		if( a instanceof AbstractBuild ) {
@@ -41,7 +30,6 @@ public abstract class LoggingFileCallable<T> implements FileCallable<T> {
 		if( action != null ) {
 			lstream = action.getLoggingStream();
 			targets = action.getTargets();
-			//remote = build.getWorkspace().isRemote();
 			
 			action.getHandler().flush();
 		}
@@ -62,21 +50,6 @@ public abstract class LoggingFileCallable<T> implements FileCallable<T> {
 		}
 	}
 
-	
-	/*
-	private LoggingFileCallable( AbstractBuild<?, ?> build ) {
-		LoggingAction action = build.getAction( LoggingAction.class );
-		if( action != null ) {
-			lstream = action.getLoggingStream();
-			targets = action.getTargets();
-			remote = build.getWorkspace().isRemote();
-			
-			action.getHandler().flush();
-		}
-	}
-	*/
-
-
 	public abstract T perform( File workspace, VirtualChannel channel ) throws IOException, InterruptedException;
 
 	@Override
@@ -95,6 +68,7 @@ public abstract class LoggingFileCallable<T> implements FileCallable<T> {
 			
 			new PrintStream( lstream.getOutputStream() ).println( "STREAM: " + lstream.getOutputStream() );
 			new PrintStream( lstream.getOutputStream() ).println( "REMOTE: " + isRemote() );
+			new PrintStream( lstream.getOutputStream() ).println( "WS: " + workspace.getAbsoluteFile() );
 			
 			/* If remote flush and close handler */
 			if( isRemote() ) {
