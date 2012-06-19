b076d3a1b997b3331fee4b4b56eefef1fc0377df
wolfgarnet <wolfgarnet@gmail.com>
2012-06-19 14:44:33 +0200

    Removed remote loggable interface

--- src/main/java/net/praqma/logging/RemoteLoggable.java
+++
@@ -1,8 +0,0 @@
-package net.praqma.logging;
-
-import hudson.FilePath;
-import hudson.model.Actionable;
-
-public abstract class RemoteLoggable extends Actionable {
-	public abstract FilePath getWorkspace();
-}
