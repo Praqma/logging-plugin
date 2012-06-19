6556218014e744a3d801ff72a9864b7f47402645
wolfgarnet <wolfgarnet@gmail.com>
2012-06-19 14:46:51 +0200

    Cleaned remote test class

--- src/main/java/net/praqma/logging/RemoteTest.java
+++ src/main/java/net/praqma/logging/RemoteTest.java
@@ -2,17 +2,12 @@ package net.praqma.logging;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.util.logging.Handler;
 import java.util.logging.Logger;
 
-import hudson.model.AbstractBuild;
 import hudson.model.Actionable;
 import hudson.remoting.VirtualChannel;
 
 public class RemoteTest extends LoggingFileCallable<Boolean> {
-
-	//private Logger logger = Logger.getLogger(RemoteTest.class.getName());
 
 	public RemoteTest( Actionable a ) {
 		super( a );
@@ -21,12 +16,15 @@ public class RemoteTest extends LoggingFileCallable<Boolean> {
 	@Override
 	public Boolean perform( File workspace, VirtualChannel channel ) throws IOException, InterruptedException {
 
-		PrintStream out = new PrintStream( lstream.getOutputStream() );
-		out.println( "Code camp 2012" );
-
 		Logger logger = Logger.getLogger( "snade.logger.remote" );
-		logger.severe( "MY CODE CAMP" );
-		logger.finest( "MY CODE CAMP" );
+		
+		logger.finest( "finest from LoggingFileCallable" );
+		logger.finer( "finer from LoggingFileCallable" );
+		logger.fine( "fine from LoggingFileCallable" );
+		logger.config( "config from LoggingFileCallable" );
+		logger.info( "info from LoggingFileCallable" );
+		logger.warning( "warning from LoggingFileCallable" );
+		logger.severe( "severe from LoggingFileCallable" );
 
 		return true;
 	}
